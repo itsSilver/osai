633536bf6100bb2b2833889cceddb9c762cd4e6d
dkaraj <dorela@hotmail.co.uk>
2022-05-21 20:20:15 +0200

    d

---
+++ mainapp/decorators/is_admin.py
@@ -0,0 +1,18 @@
+from functools import wraps
+from django.http import HttpResponseRedirect
+from django.http import JsonResponse
+
+
+def is_admin(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+
+        type = request.user.is_admin
+        if type:
+            return function(request, *args, **kwargs)
+        else:
+            return JsonResponse({'message': 'You must be admin to proceed'})
+
+    return wrap
+
+
