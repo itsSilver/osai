5ba51c8397451db0f9275b695d9af8901bbb84c8
Marvi-G <74012483+Marvi-G@users.noreply.github.com>
2022-06-13 14:19:37 +0200

    Added login logo to the login

--- client/pages/login/index.vue
+++ client/pages/login/index.vue
@@ -9,34 +9,54 @@
               <div class="col-lg-6">
                 <div class="card-body p-md-5 mx-md-4">
                   <div class="text-center">
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                      style="width: 185px" alt="logo" />
-                    <h4 class="mt-1 mb-5 pb-1">OSAI</h4>
+                    <img
+                      src="@/assets/images/logo.png"
+                      style="width: 185px"
+                      alt="logo"
+                    />
+                    <h4 class="mt-1 mb-5 pb-1"></h4>
                   </div>
 
                   <b-form @submit.prevent="submitForm">
                     <p>Please login to your account</p>
 
                     <div class="form-outline mb-4">
-                      <b-form-input type="email" id="form2Example11" class="form-control"
-                        placeholder="Phone number or email address" v-model="form.email" />
-                      <label class="form-label" for="form2Example11">Username</label>
+                      <b-form-input
+                        type="email"
+                        id="form2Example11"
+                        class="form-control"
+                        placeholder="Phone number or email address"
+                        v-model="form.email"
+                      />
+                      <label class="form-label" for="form2Example11"
+                        >Username</label
+                      >
                     </div>
 
                     <div class="form-outline mb-4">
-                      <b-form-input type="password" id="form2Example22" class="form-control" placeholder="Password"
-                        v-model="form.password" />
-                      <label class="form-label" for="form2Example22">Password</label>
+                      <b-form-input
+                        type="password"
+                        id="form2Example22"
+                        class="form-control"
+                        placeholder="Password"
+                        v-model="form.password"
+                      />
+                      <label class="form-label" for="form2Example22"
+                        >Password</label
+                      >
                     </div>
 
                     <div class="text-center pt-1 mb-5 pb-1">
-                      <button class="
+                      <button
+                        class="
                           btn btn-primary btn-block
                           fa-lg
                           gradient-custom-2
                           mb-3
                           login-button
-                        " type="submit">
+                        "
+                        type="submit"
+                      >
                         Log in
                       </button>
                       <a class="text-muted" href="#!">Forgot password?</a>
@@ -78,7 +98,8 @@ export default {
   },
   methods: {
     async submitForm() {
-      this.$auth.loginWith('local', { data: this.form })
+      this.$auth
+        .loginWith('local', { data: this.form })
         .then((response) => {
           // this.token = response.data.token
           // this.$auth.strategy.token.set(this.token)
@@ -92,7 +113,7 @@ export default {
         //     this.redirectPage()
         //   })
         .catch((err) => {
-          console.log('err', err);
+          console.log('err', err)
         })
     },
   },
