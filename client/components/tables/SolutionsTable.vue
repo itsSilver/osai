54b1c151db6ba8d4833526a287385f38781dc052
Marvi-G <74012483+Marvi-G@users.noreply.github.com>
2022-07-06 12:29:18 +0200

    Added permissions for the soluzioni

--- client/components/tables/SolutionsTable.vue
+++ client/components/tables/SolutionsTable.vue
@@ -154,18 +154,21 @@
           <b-button
             class="mx-1 view-btn"
             @click="pushRoute(`view/${props.row.id}`)"
+            v-permission="'Can view soluzioni'"
           >
             <i class="mdi mdi-eye"></i>
           </b-button>
           <b-button
             class="mx-1 edit-btn"
             @click="pushRoute(`update/${props.row.id}`)"
+            v-permission="'Can change soluzioni'"
           >
             <i class="mdi mdi-pencil"></i>
           </b-button>
           <b-button
             class="mx-1 delete-btn"
             @click="deleteDocument(props.row.id)"
+            v-permission="'Can delete soluzioni'"
           >
             <i class="mdi mdi-delete"></i>
           </b-button>
