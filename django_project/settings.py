a1e3d4d482707caf8f765301b45c61261e3ac813
Silver <thesilvervevo@gmail.com>
2022-07-12 15:44:10 +0200

    added django logger

--- django_project/settings.py
+++ django_project/settings.py
@@ -172,3 +172,33 @@ MEDIA_ROOT = os.path.join(Path(__file__).resolve().parent.parent, 'media')
 
 # URL used to access the media
 MEDIA_URL = '/media/'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "./django.log",
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django_project": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-8s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+}
